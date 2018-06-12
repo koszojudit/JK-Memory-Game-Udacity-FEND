9b470e0ad0d5a1cc91f7e61667be3a25fd037273
Judit Koszo <koszojudit@hotmail.com>
2018-06-12 21:54:37 +0200

    Shuffle cards and display symbols, add timer

---
+++ js/app.js
@@ -0,0 +1,119 @@
+
+
+
+
+const cards = document.querySelectorAll('.deck li');
+
+function startGame() {
+  for (let card of cards) {
+      card.className = "card show";
+      card.isClicked = 0;
+  }
+  shuffleDeck(cards); 
+}
+
+
+
+/*
+ * Create a list that holds all of your cards
+ */
+
+ const listOfCards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt",
+     "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb",
+     "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// Shuffle deck
+function shuffleDeck (deck) {
+    let shuffledListOfCards = shuffle(listOfCards);
+    for (i = 0; i < deck.length; i++) {
+        deck[i].firstElementChild.className = shuffledListOfCards[i];
+    }
+}
+
+startGame();
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+
+
+
+/* TODOs:
+
+ * 1. Page onLoad:
+ *   - shuffle cards randomly
+ *   - display cards
+
+ * 2. Card event Listeners
+ *   - hover animation
+ *   - click event effects:
+ *      - if first click: start timer
+ *      - check if new card is clicked, if yes, increase move count by 1
+ *      - put card to open status
+ *         - animation (turn)
+ *         - display icon
+ *         - change bgrd color
+ *   - matching logic (if open card list is not empty, amtch symbols)
+ *      - if match:
+ *         - keep cards open
+ *         - change bgrd color
+ *      - if does not match:
+ *         - remove from list of open cards
+ *         - hide symbol, animation, default bgrd color
+
+ * 3. Move counter
+ *   - increase by 1 if a new card is clicked
+
+ * 4. Stars
+ *   -  display 3 stars full
+ *   -  decrease by 1 if a certain condition is met in move count
+
+ * 5. Timer
+ *   - start when first card is clicked and put to open status
+ *   - stop when all cards are open and matched
+
+ * 6. Congratulations popup
+ *   - define winning condition (when all cards are open and matched)
+ *   - render popup
+ *   - display current status of:
+ *      - nr of moves
+ *      - time
+ *      - stars
+ *   - Want to play again?
+
+ * 7. Restart button
+ *   - reset gameboard (cards in closed status, reshuffled)
+ *   - reset timer to 00:00:00:00
+ *   - reset stars to 3 full
+ */
